4b4b254ee546aa6e9f805a45f64b007e3f82c6d5
Aaron Smith <magik_smith@outlook.com>
2020-04-14 20:11:39 -0400

    Added support for attack animations, worked on support for oversize anims

--- public/main.js
+++ public/main.js
@@ -1,71 +1,141 @@
 "use strict";
 console.log("hello!");
-// Currnet State of the player's animation
+// Global Static class to keep track of input
+class Input {
+    constructor() {
+        throw new TypeError('Input is a static class and cannot be instatianted.');
+    }
+}
+// Current State of the player's animation
 var AnimState;
 (function (AnimState) {
     AnimState[AnimState["STATE_WALKING"] = 0] = "STATE_WALKING";
+    AnimState[AnimState["STATE_ATTACKING"] = 1] = "STATE_ATTACKING";
 })(AnimState || (AnimState = {}));
 ;
+var Directions;
+(function (Directions) {
+    Directions["Down"] = "down";
+    Directions["Up"] = "up";
+    Directions["Left"] = "left";
+    Directions["Right"] = "right";
+})(Directions || (Directions = {}));
+var LPCAnims;
+(function (LPCAnims) {
+    LPCAnims["spell_cast"] = "spell_cast";
+    LPCAnims["thrust"] = "thrust";
+    LPCAnims["walk"] = "walk";
+    LPCAnims["slash"] = "slash";
+    LPCAnims["shoot"] = "shoot";
+})(LPCAnims || (LPCAnims = {}));
 class Player {
-    constructor(sprite, textureName) {
+    constructor(sprite, textureName, loadOversizeAnims = false) {
         this.sprite = sprite;
         this.textureName = textureName;
         this.animState = AnimState.STATE_WALKING;
+        this.animParameters = {
+            direction: Directions.Down
+        };
+        this.attackRate = 1;
+        this.speed = 8;
         this.createLpcAnimDatabase(textureName, [
-            { name: 'spell_cast', numOfFrames: 7 },
-            { name: 'thrust', numOfFrames: 8 },
-            { name: 'walk', numOfFrames: 9 },
-            { name: 'slash', numOfFrames: 6 },
-            { name: 'shoot', numOfFrames: 13 },
-        ]);
+            { name: 'spell_cast', numOfFrames: 7, loop: 1 },
+            { name: 'thrust', numOfFrames: 8, loop: 1 },
+            { name: 'walk', numOfFrames: 9, loop: -1 },
+            { name: 'slash', numOfFrames: 6, loop: 1 },
+            { name: 'shoot', numOfFrames: 13, loop: 1 },
+        ], loadOversizeAnims);
     }
     // Create a database of animations for our sprite
-    createLpcAnimDatabase(textureName, listOfAnims) {
+    createLpcAnimDatabase(textureName, listOfAnims, loadOverizeAnims) {
         // This is to account for the blank spots since not all anims take up a full row
         var maxRowSize = 24;
-        var directions = ["up", "right", "down", "left"];
+        var oversizeAnimName = '';
+        var directions = ["up", "left", "down", "right"];
         var lastIndex = 0;
         for (let i = 0; i < listOfAnims.length; i++) {
             let currAnim = listOfAnims[i];
-            let newReel = {};
             for (let d = 0; d < 4; d++) {
-                newReel[directions[d]] = [];
+                let reel = [];
                 for (let r = lastIndex; r < lastIndex + maxRowSize; r++) {
                     if (r < currAnim.numOfFrames + lastIndex) {
-                        newReel[directions[d]].push(r);
+                        reel.push(r);
                     }
                 }
                 lastIndex += maxRowSize;
+                // console.log(`${ textureName }_${ currAnim.name }_${directions[d]} = ${reel}`);
                 // Anims are set using the following pattern: name_animation_direction (eg: wizard_walk_up)
-                if (!game.anims.create({ key: `${textureName}_${currAnim.name}_${directions[d]}`, frames: game.anims.generateFrameNumbers(textureName, newReel[directions[d]]) })) {
-                    throw new Error(`Create anim failed! key=${textureName}_${currAnim.name}_${directions[d]}`);
+                if (!currAnim.hasOverize) {
+                    if (!game.anims.create({ key: `${textureName}_${currAnim.name}_${directions[d]}`, frames: game.anims.generateFrameNumbers(textureName, { frames: reel }), repeat: currAnim.loop })) {
+                        throw new Error(`Create anim failed!`);
+                    }
                 }
+                else if (oversizeAnimName === '') {
+                    oversizeAnimName = currAnim.name;
+                }
+                else
+                    console.warn('Can only have one anim marked as hasOversize, the anim ' + currAnim.name + 'will fail to load');
             }
         }
     }
-    // Updates the player's animation based on his animation state
-    updateAnimation() {
+    update(time, delta) {
+        this.getNextAnimatorState();
+        this.animate();
+        this.move(delta);
+    }
+    move(delta) {
+        this.sprite.setVelocity(0, 0);
+        // Horizontal Movement
+        if (Input.cursors.left.isDown) {
+            this.sprite.setVelocityX(-this.speed * delta);
+            this.animParameters.direction = Directions.Left;
+        }
+        else if (Input.cursors.right.isDown) {
+            this.sprite.setVelocityX(this.speed * delta);
+            this.animParameters.direction = Directions.Right;
+        }
+        // Vertical Movement
+        if (Input.cursors.up.isDown) {
+            this.sprite.setVelocityY(-this.speed * delta);
+            this.animParameters.direction = Directions.Up;
+        }
+        else if (Input.cursors.down.isDown) {
+            this.sprite.setVelocityY(this.speed * delta);
+            this.animParameters.direction = Directions.Down;
+        }
+    }
+    // updates the animator state machine
+    getNextAnimatorState() {
         switch (this.animState) {
             case AnimState.STATE_WALKING:
-                // Update the animation last and give left/right animations precedence over up/down animations
-                if (this.sprite.body.velocity.x < 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_left', true);
+                if (Input.keyboard.checkDown(Input.attackKey, this.attackRate * 1000)) {
+                    this.animState = AnimState.STATE_ATTACKING;
                 }
-                else if (this.sprite.body.velocity.x > 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_right', true);
+                break;
+            case AnimState.STATE_ATTACKING:
+                if (this.sprite.anims.getProgress() >= 1) {
+                    this.animState = AnimState.STATE_WALKING;
                 }
-                else if (this.sprite.body.velocity.y < 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_up', true);
-                }
-                else if (this.sprite.body.velocity.y > 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_down', true);
-                }
-                else {
+                break;
+        }
+    }
+    // Updates the player's animation based on his animation state
+    animate() {
+        switch (this.animState) {
+            case AnimState.STATE_WALKING:
+                if (this.sprite.body.velocity.x !== 0 || this.sprite.body.velocity.y !== 0)
+                    this.playAnim(LPCAnims.walk);
+                else
                     this.sprite.anims.stop();
-                }
+                break;
+            case AnimState.STATE_ATTACKING:
+                this.playAnim(LPCAnims.thrust);
                 break;
             default: throw new Error('Invalid Anim State!');
         }
+    }
+    playAnim(name, ignoreIfPlaying = true) {
+        this.sprite.play(`${this.textureName}_${name}_${this.animParameters.direction}`, ignoreIfPlaying);
     }
 }
 class TileMap {
@@ -85,7 +155,11 @@ class BootScene extends Phaser.Scene {
         this.load.tilemapTiledJSON('map', 'assets/map/map.json');
         // our two characters
         this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+        // Load the image twice: once for regular anims and second for oversize
+        // Anims are in 64x64 boxes with 24 boxes per row
         this.load.spritesheet('wizard', 'assets/LPC_black_wizard_m.png', { frameWidth: 64, frameHeight: 64 });
+        // Oversize anims start on frame 168
+        this.load.spritesheet('wizard_oversize', 'assets/LPC_black_wizard_m.png', { frameWidth: 64 * 3, frameHeight: 64 * 3 });
     }
     ;
     create() {
@@ -102,9 +176,10 @@ class WorldScene extends Phaser.Scene {
     }
     create() {
         this.createMap();
-        // Create Animations
-        this.player = this.createPlayer();
-        this.cursors = this.input.keyboard.createCursorKeys();
+        this.createPlayer();
+        Input.cursors = this.input.keyboard.createCursorKeys();
+        Input.keyboard = this.input.keyboard;
+        Input.attackKey = Input.keyboard.addKey('SPACE', true);
     }
     createMap() {
         this.map = this.make.tilemap({ key: 'map' });
@@ -118,25 +193,9 @@ class WorldScene extends Phaser.Scene {
         this.physics.world.bounds.width = this.map.widthInPixels;
         this.physics.world.bounds.height = this.map.heightInPixels;
         this.player.sprite.setCollideWorldBounds(true);
-        return this.player;
     }
     update(time, delta) {
-        this.player.sprite.setVelocity(0, 0);
-        // Horizontal Movement
-        if (this.cursors.left.isDown) {
-            this.player.sprite.setVelocityX(-80);
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.sprite.setVelocityX(80);
-        }
-        // Vertical Movement
-        if (this.cursors.up.isDown) {
-            this.player.sprite.setVelocityY(-80);
-        }
-        else if (this.cursors.down.isDown) {
-            this.player.sprite.setVelocityY(80);
-        }
-        this.player.updateAnimation();
+        this.player.update(time, delta);
     }
 }
 ;
--- public/main.ts
+++ public/main.ts
@@ -1,80 +1,179 @@
 ﻿console.log("hello!");
 
 
-// Currnet State of the player's animation
+// Global Static class to keep track of input
+class Input {
+    static keyboard: Phaser.Input.Keyboard.KeyboardPlugin;
+    static cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    static attackKey: Phaser.Input.Keyboard.Key;
+
+    constructor()
+    {
+        throw new TypeError('Input is a static class and cannot be instatianted.');
+    }
+
+
+}
+
+// Current State of the player's animation
 enum AnimState
 {
-    STATE_WALKING
+    STATE_WALKING,
+    STATE_ATTACKING
 };
 
+enum Directions
+{
+    Down = 'down',
+    Up = 'up',
+    Left = 'left',
+    Right = 'right'
+
+}
+enum LPCAnims
+{
+    spell_cast = 'spell_cast',
+    thrust = 'thrust',
+    walk = 'walk',
+    slash = 'slash',
+    shoot = 'shoot'
+}
+
+interface AnimData
+{
+    name: string
+    numOfFrames: number
+    loop?: number;
+    hasOverize?: boolean;
+}
 class Player
 {
     animState: AnimState = AnimState.STATE_WALKING;
+    animParameters = {
+        direction: Directions.Down
+    };
+    attackRate =  1;
+    speed = 8;
 
-    constructor(public sprite: Phaser.Physics.Arcade.Sprite, public textureName: string)
+    constructor(public sprite: Phaser.Physics.Arcade.Sprite, public textureName: string, loadOversizeAnims: boolean = false)
     {
         this.createLpcAnimDatabase(textureName, [
-            { name: 'spell_cast', numOfFrames: 7 },
-            { name: 'thrust', numOfFrames: 8 },
-            { name: 'walk', numOfFrames: 9 },
-            { name: 'slash', numOfFrames: 6 },
-            { name: 'shoot', numOfFrames: 13 },
-        ]);
-
+            { name: 'spell_cast', numOfFrames: 7, loop: 1 },
+            { name: 'thrust', numOfFrames: 8, loop: 1 },
+            { name: 'walk', numOfFrames: 9, loop: -1 },
+            { name: 'slash', numOfFrames: 6, loop: 1 },
+            { name: 'shoot', numOfFrames: 13, loop: 1 },
+        ], loadOversizeAnims);
     }
-
+    
     // Create a database of animations for our sprite
-    createLpcAnimDatabase(textureName: string, listOfAnims: { name: string, numOfFrames: number }[]) {
-
+    createLpcAnimDatabase(textureName: string, listOfAnims: AnimData[], loadOverizeAnims: boolean) {
         // This is to account for the blank spots since not all anims take up a full row
         var maxRowSize = 24;
-
-        var directions = ["up", "right", "down", "left"];
+        var oversizeAnimName = '';
+        var directions = ["up", "left", "down", "right"];
 
         var lastIndex = 0;
         for (let i = 0; i < listOfAnims.length; i++) {
             let currAnim = listOfAnims[i];
-            let newReel: any = {}
 
             for (let d = 0; d < 4; d++) {
-                newReel[directions[d]] = [];
+                let reel: number[] = [];
+
                 for (let r = lastIndex; r < lastIndex + maxRowSize; r++) {
                     if (r < currAnim.numOfFrames + lastIndex) {
-                        newReel[directions[d]].push(r);
+                        reel.push(r);
                     }
                 }
                 lastIndex += maxRowSize;
-
+                // console.log(`${ textureName }_${ currAnim.name }_${directions[d]} = ${reel}`);
                 // Anims are set using the following pattern: name_animation_direction (eg: wizard_walk_up)
-                if (!game.anims.create({ key: `${textureName}_${currAnim.name}_${directions[d]}`, frames: game.anims.generateFrameNumbers(textureName, newReel[directions[d]]) })) {
-                    throw new Error(`Create anim failed! key=${textureName}_${currAnim.name}_${directions[d]}`);
-                }
+                if (!currAnim.hasOverize) {
+                    if (!game.anims.create({ key: `${textureName}_${currAnim.name}_${directions[d]}`, frames: game.anims.generateFrameNumbers(textureName, { frames: reel }), repeat: currAnim.loop })) {
+                        throw new Error(`Create anim failed!`);
+                    }
+                } else if (oversizeAnimName === '') {
+                    oversizeAnimName = currAnim.name;
+                } else console.warn('Can only have one anim marked as hasOversize, the anim ' + currAnim.name + 'will fail to load');
             }
         }
     }
+
+    update(time: number, delta: number)
+    {
+        this.getNextAnimatorState();
+        this.animate();
+        this.move(delta);
+    }
+
+    move(delta: number)
+    {
+        this.sprite.setVelocity(0, 0);
+
+        // Horizontal Movement
+        if (Input.cursors.left.isDown) {
+            this.sprite.setVelocityX(-this.speed * delta);
+            this.animParameters.direction = Directions.Left;
+        } else if (Input.cursors.right.isDown) {
+            this.sprite.setVelocityX(this.speed * delta);
+            this.animParameters.direction = Directions.Right;
+        }
+
+        // Vertical Movement
+        if (Input.cursors.up.isDown) {
+            this.sprite.setVelocityY(-this.speed * delta);
+            this.animParameters.direction = Directions.Up;
+        } else if (Input.cursors.down.isDown) {
+            this.sprite.setVelocityY(this.speed * delta);
+            this.animParameters.direction = Directions.Down;
+        }
+    }
+
+    // updates the animator state machine
+    getNextAnimatorState()
+    {
+        switch (this.animState)
+        {
+            case AnimState.STATE_WALKING:
+                if (Input.keyboard.checkDown(Input.attackKey, this.attackRate * 1000))
+                {
+                    this.animState = AnimState.STATE_ATTACKING;
+                }
+                break;
+            case AnimState.STATE_ATTACKING:
+                if (this.sprite.anims.getProgress() >= 1)
+                {
+                    this.animState = AnimState.STATE_WALKING;
+                }
+                break;
+        }
+        
+    }
+
     // Updates the player's animation based on his animation state
-    updateAnimation()
+    animate()
     {
         switch (this.animState) {
 
             case AnimState.STATE_WALKING:
-                // Update the animation last and give left/right animations precedence over up/down animations
-                if (this.sprite.body.velocity.x < 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_left', true);
-                } else if (this.sprite.body.velocity.x > 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_right', true);
-                } else if (this.sprite.body.velocity.y < 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_up', true);
-                } else if (this.sprite.body.velocity.y > 0) {
-                    this.sprite.anims.play(this.textureName + '_walk_down', true);
-                } else {
+                if (this.sprite.body.velocity.x !== 0 || this.sprite.body.velocity.y !== 0)
+                    this.playAnim(LPCAnims.walk);
+                else
                     this.sprite.anims.stop();
-                }
                 break;
+
+            case AnimState.STATE_ATTACKING:
+                this.playAnim(LPCAnims.thrust);
+                break;
+
             default: throw new Error('Invalid Anim State!');
         }
     }
 
+    playAnim(name: LPCAnims, ignoreIfPlaying = true)
+    {
+        this.sprite.play(`${this.textureName}_${name}_${this.animParameters.direction}`, ignoreIfPlaying);
+    }
 }
 
 class TileMap
@@ -98,14 +197,19 @@ class BootScene extends Phaser.Scene {
 
 	preload() {
 		// map tiles
-		this.load.image('tiles', 'assets/map/spritesheet.png');
+        this.load.image('tiles', 'assets/map/spritesheet.png');
 
 		// map in json format
-		this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+        this.load.tilemapTiledJSON('map', 'assets/map/map.json');
 
 		// our two characters
         this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+
+        // Load the image twice: once for regular anims and second for oversize
+        // Anims are in 64x64 boxes with 24 boxes per row
         this.load.spritesheet('wizard', 'assets/LPC_black_wizard_m.png', { frameWidth: 64, frameHeight: 64 });
+        // Oversize anims start on frame 168
+        this.load.spritesheet('wizard_oversize', 'assets/LPC_black_wizard_m.png', { frameWidth: 64 * 3, frameHeight: 64 * 3 });
 	};
 
 	create() {
@@ -120,7 +224,7 @@ class WorldScene extends Phaser.Scene {
     obstacles: Phaser.Tilemaps.StaticTilemapLayer;
 
     player: Player;
-    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    
 
     constructor()
     {
@@ -134,12 +238,11 @@ class WorldScene extends Phaser.Scene {
     create() {
 
         this.createMap();
+        this.createPlayer();
 
-        // Create Animations
-
-        this.player = this.createPlayer();
-
-        this.cursors = this.input.keyboard.createCursorKeys();
+        Input.cursors = this.input.keyboard.createCursorKeys();
+        Input.keyboard = this.input.keyboard;
+        Input.attackKey = Input.keyboard.addKey('SPACE', true);
 	}
 
     createMap()
@@ -162,29 +265,12 @@ class WorldScene extends Phaser.Scene {
 
         this.player.sprite.setCollideWorldBounds(true);
 
-        return this.player; 
 	}
 
     
-    update(time: number, delta: number) {
-
-        this.player.sprite.setVelocity(0, 0);
-
-		// Horizontal Movement
-		if (this.cursors.left.isDown) {
-            this.player.sprite.setVelocityX(-80);
-		} else if (this.cursors.right.isDown) {
-            this.player.sprite.setVelocityX(80);
-		}
-
-		// Vertical Movement
-        if (this.cursors.up.isDown) {
-            this.player.sprite.setVelocityY(-80);
-        } else if (this.cursors.down.isDown) {
-            this.player.sprite.setVelocityY(80);
-        }
-
-        this.player.updateAnimation();
+    update(time: number, delta: number)
+    {
+        this.player.update(time, delta);
 	}
 };
 
